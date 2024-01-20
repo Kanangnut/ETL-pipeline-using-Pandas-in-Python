f989cfb8e4c1eb76412843b3db14d5c7043facab
Cat-Kanangnut <130201193+Kanangnut@users.noreply.github.com>
2024-01-20 10:57:17 +0800

    Update pythonFile.py

--- pythonFile.py
+++ pythonFile.py
@@ -7,13 +7,13 @@ response_list = []
 API_KEY = config.api_key
 df = ""
 
-# create a loop that requests each movie one at a time and appends the response to a list.
+#create a loop that requests each movie one at a time and appends the response to a list.
 for movie_id in range(11, 996):
-    # send a single GET request to the API,  receive a JSON record
+    #send a single GET request to the API,  receive a JSON record
     r = requests.get(
         'https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id, API_KEY))
     response_list.append(r.json())
-# print (response_list)
+#print (response_list)
 
 df = pd.DataFrame.from_dict(response_list)
 df.to_csv('dataset.csv', index=False)
